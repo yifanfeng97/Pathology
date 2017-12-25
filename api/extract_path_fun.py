d418ab4a97d4e09bf55ed1e318ec5cb88bbb9da9
fengyifan <1106132246@qq.com>
2017-12-25 16:20:47 +0800

    writing

---
+++ api/extract_path_fun.py
@@ -0,0 +1,4 @@
+import slide_fun
+
+def extract(data, type, auto_save_patch = True):
+    pass
--- api/patch_fun.py
+++ api/patch_fun.py
@@ -1,6 +1,24 @@
 import slide_fun
 import config_fun
 import json
+import extract_path_fun
+import os
+from tqdm import tqdm
+
+def _prepare_patch(data, auto_save_patch = True):
+    patches = []
+    for idx, item in enumerate(tqdm(data)):
+        patch = []
+        if 'tumor' in item['info']:
+            patch = extract_path_fun.extract(item, 'pos', auto_save_patch = auto_save_patch)
+        else:
+            patch = extract_path_fun.extract(item, 'neg', auto_save_patch = auto_save_patch)
+        patches.append({'data': item['data'][0],
+                        'info': item['info'], 'patch':patch})
+        print('get patches from %s, pos:%d, neg:%d\n'%
+              (os.path.basename(item['data'][0]), len(patch['pos']), len(patch['neg'])))
+    return patches
+
 
 def generate_patch(auto_save_patch = True):
     cfg = config_fun.config()
@@ -14,5 +32,7 @@ def generate_patch(auto_save_patch = True):
     test_data  = filter(lambda item: item['info'] == 'test_tumor' or
                                     item['info'] == 'test_normal', split_data)
 
+    train_patch = _prepare_patch(train_data, auto_save_patch = auto_save_patch)
+    val_patch = _prepare_patch(val_data, auto_save_patch = auto_save_patch)
 
 
