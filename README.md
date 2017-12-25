0b74f90fc5d555b11dc8803085c0b66d891c8c35
fengyifan <1106132246@qq.com>
2017-12-25 14:08:58 +0800

    writing

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+## the struct of divided data
+
+- data
+    - [0] path of the file
+    - [1] None or the mask files of data
+- info
+    - val_tumor
+    - val_normal
+    - train_tumor
+    - train_normal
+    - test_tumor
+    - test_normal
--- api/patch_fun.py
+++ api/patch_fun.py
@@ -1,8 +1,18 @@
 import slide_fun
 import config_fun
+import json
 
 def generate_patch(auto_save_patch = True):
-    pass
+    cfg = config_fun.config()
+    with open(cfg.split_file) as f:
+        split_data = json.load(f)
+
+    train_data = filter(lambda item: item['info'] == 'train_tumor' or
+                                    item['info'] == 'train_normal', split_data)
+    val_data   = filter(lambda item: item['info'] == 'val_tumor' or
+                                    item['info'] == 'val_normal', split_data)
+    test_data  = filter(lambda item: item['info'] == 'test_tumor' or
+                                    item['info'] == 'test_normal', split_data)
 
 
 
