2cdcb11c6b4bfcc9bfa19caea73c7ce099e8d1e1
fengyifan <1106132246@qq.com>
2017-12-26 11:45:53 +0800

    writing

--- api/extract_patch_fun.py
+++ api/extract_patch_fun.py
@@ -1,5 +1,17 @@
 import slide_fun
 import config_fun
+import random
+import numpy as np
+import scipy.ndimage as ndimage
+from PIL import Image
+from skimage.morphology import dilation, star
+from skimage.filters import threshold_otsu
+from itertools import product
+
+BACKGROUND = 0
+SELECTED = 1
+NORMAL = 2
+TUMOR = 3
 
 
 class single_img_process():
@@ -11,17 +23,17 @@ class single_img_process():
         self._type = type
 
         self._img = slide_fun.AllSlide(self._file_name)
-        self._raw_mask = None
-        self._raw_size = self._img.level_dimensions[0]
+        self._max_mask = None
+        self._max_mask_size = self._img.level_dimensions[0]
 
-    def _2048_level(self):
+    def _get_level(self, size):
         level = self._img.level_count -1
-        while self._img.level_dimensions[level][0] < 2048 and \
-            self._img.level_dimensions[level][1] < 2048:
+        while self._img.level_dimensions[level][0] < size and \
+            self._img.level_dimensions[level][1] < size:
             level -= 1
         return level
 
-    def _threshold_downsample_lvl(self, lvl_img):
+    def _threshold_downsample_lvl(self, img):
         """Generates thresholded overview image.
 
         Args:
@@ -32,17 +44,17 @@ class single_img_process():
         """
 
         # calculate the overview level size and retrieve the image
-        downsample_img = lvl_img.convert('HSV')
-        downsample_img = np.array(downsample_img)
+        img_hsv = img.convert('HSV')
+        img_hsv_np = np.array(img_hsv)
 
         # dilate image and then threshold the image
-        schannel = downsample_img[:, :, 1]
+        schannel = img_hsv_np[:, :, 1]
         schannel = dilation(schannel, star(3))
         schannel = ndimage.gaussian_filter(schannel, sigma=(5, 5), order=0)
         threshold_global = threshold_otsu(schannel)
 
-        schannel[schannel > threshold_global] = 255
-        schannel[schannel <= threshold_global] = 0
+        schannel[schannel > threshold_global] = SELECTED
+        schannel[schannel <= threshold_global] = BACKGROUND
 
         # import scipy.misc   # check the result
         # scipy.misc.imsave('outfile.jpg', schannel)
@@ -50,8 +62,10 @@ class single_img_process():
         return schannel
 
     def _generate_mask(self):
-        self._ov_mask = None
-        self._ov_mask_size = self._2048_level()
+        self._min_mask = None
+        self._min_mask_size = self._get_level(2048)
+
+        self._max_mask = self._threshold_downsample_lvl()
 
 
     def _save_random_mask_and_patch(self):
